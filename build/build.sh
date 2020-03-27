2002da1b4dabee85d5eb8c1446938bb939d7e15c
Zybjana Bedo <zbedo@broadinstitute.org>
2020-03-27 15:19:39 -0400

    edit build.sh

--- build/build.sh
+++ build/build.sh
@@ -4,12 +4,11 @@ set -e
 
 : ${GCLOUD_REGISTRY:=us.gcr.io}
 : ${IMAGE:=$GITHUB_REPOSITORY}
-: ${ARGS:=} 
 : ${TAG:=$GITHUB_SHA}
 : ${DEFAULT_BRANCH_TAG:=true}
 : ${LATEST:=true}
 
-docker build $ARGS -t $IMAGE:$TAG .
+docker build -t $IMAGE:$TAG .
 docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$IMAGE:$TAG
 
 if [ $LATEST = true ]; then
@@ -18,7 +17,7 @@ fi
 
 if [ "$DEFAULT_BRANCH_TAG" = "true" ]; then
   BRANCH=$(echo $GITHUB_REF | rev | cut -f 1 -d / | rev)
-  if [ "$BRANCH" = "master" ]; then # TODO
+  if [ "$BRANCH" = "master" ]; then 
     docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$IMAGE:$BRANCH
   fi
 fi
