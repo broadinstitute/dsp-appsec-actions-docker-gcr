59216b09b26326fc1e3b35535f426fca654a2517
Zybjana Bedo <zbedo@broadinstitute.org>
2020-04-02 22:10:02 -0400

    [dockerhub] Add script to build images

---
+++ build_dockerhub/build_dockerhub.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+: ${IMAGE:=$GITHUB_REPOSITORY}
+: ${TAG:=$GITHUB_SHA}
+: ${DEFAULT_BRANCH_TAG:=true}
+: ${LATEST:=true}
+
+docker build -t $IMAGE:$TAG .
+
+if [ $LATEST = true ]; then
+  docker tag $IMAGE:$TAG $IMAGE:latest
+else 
+  docker tag $IMAGE:$TAG $IMAGE:$TAG
+fi
+
+if [ "$DEFAULT_BRANCH_TAG" = "true" ]; then
+  BRANCH=$(echo $GITHUB_REF | rev | cut -f 1 -d / | rev)
+  if [ "$BRANCH" = "master" ]; then 
+    docker tag $IMAGE:$TAG $IMAGE:$BRANCH
+  fi
+fi
