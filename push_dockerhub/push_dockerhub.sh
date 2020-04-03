7c872cd6363b216cd6271366adad83aa33cf48ba
Zybjana Bedo <zbedo@broadinstitute.org>
2020-04-02 21:57:42 -0400

    Add script to push images to Dockerhub

---
+++ push_dockerhub/push_dockerhub.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+: ${IMAGE:=$GITHUB_REPOSITORY}
+: ${TAG:=$GITHUB_SHA}
+: ${DEFAULT_BRANCH_TAG:=true}
+: ${LATEST:=true}
+
+if [ -n "${DOCKERHUB_USERNAME}" ]; then
+  docker login --username=${DOCKERHUB_USERNAME} --password=${DOCKERHUB_PASSWORD}}
+else
+  echo "DOCKERHUB_USERNAME was empty, not performing auth, please set a secret in your repo" 1>&2
+fi
+
+if [ $LATEST = true ]; then
+  docker push $DOCKERHUB_USERNAME/$IMAGE:latest
+else
+  docker push $DOCKERHUB_USERNAME/$IMAGE:$TAG
+fi
