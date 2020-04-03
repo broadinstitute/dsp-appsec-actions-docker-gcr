858d4ff731226024ba03e90cc7fb3e41b281e8ca
Zybjana Bedo <zbedo@broadinstitute.org>
2020-04-02 22:46:06 -0400

    Rename image for dockerhub

--- build_dockerhub/build_dockerhub.sh
+++ build_dockerhub/build_dockerhub.sh
@@ -7,17 +7,17 @@ set -euo pipefail
 : ${DEFAULT_BRANCH_TAG:=true}
 : ${LATEST:=true}
 
-docker build -t $IMAGE:$TAG .
+docker build -t $DOCKERHUB_USERNAME/$IMAGE:$TAG .
 
 if [ $LATEST = true ]; then
-  docker tag $IMAGE:$TAG $IMAGE:latest
+  docker tag $DOCKERHUB_USERNAME/$IMAGE:$TAG $DOCKERHUB_USERNAME/$IMAGE:latest
 else 
-  docker tag $IMAGE:$TAG $IMAGE:$TAG
+  docker tag $DOCKERHUB_USERNAME/$IMAGE:$TAG $DOCKERHUB_USERNAME/$IMAGE:$TAG
 fi
 
 if [ "$DEFAULT_BRANCH_TAG" = "true" ]; then
   BRANCH=$(echo $GITHUB_REF | rev | cut -f 1 -d / | rev)
   if [ "$BRANCH" = "master" ]; then 
-    docker tag $IMAGE:$TAG $IMAGE:$BRANCH
+    docker tag $DOCKERHUB_USERNAME/$IMAGE:$TAG $DOCKERHUB_USERNAME/$IMAGE:$BRANCH
   fi
 fi
